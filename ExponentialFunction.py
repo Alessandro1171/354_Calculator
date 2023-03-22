81458d9c74e6ce20cc5ca58ecacda3a675218890
Cal Monica Chen <monicachen808@gmail.com>
2023-03-22 13:39:49 -0400

    continued exponential func

--- ExponentialFunction.py
+++ ExponentialFunction.py
@@ -1,13 +1,31 @@
 # Function - y = ab^x
 
-def check_decimal(x):
-    decimal_digits = len(str(x).split('.')[1])
-    if decimal_digits > 5:
-        return True
-    else:
-        return False
 
-def exponential_function(base=float, exponent=float):
+def truncate_value(value):
+    # truncates value to 5 values after decimal point
+    final_value = '%.5f' % value
+    return final_value
+
+
+def check_decimal(num):
+    if num < 0.0001:
+        return 0.0001
+    decimal_digits = len(str(num).split('.')[1])
+    if decimal_digits > 5:
+        final_num = truncate_value(num)
+        return float(final_num)
+    else:
+        return num
+
+
+def exponential_function(base=float, exponent_a=float):
+    # TODO: CUT THE NUMBER TO MAX OF 5 AFTER DECIMAL POINT
+    array = str(exponent_a).split('.')
+    if len(array) == 2:
+        # values after decimal exists
+        exponent = check_decimal(exponent_a)
+    else:
+        exponent = exponent_a
     if base == 0:
         return 0
     elif exponent == 0:
@@ -15,8 +33,10 @@ def exponential_function(base=float, exponent=float):
     elif exponent == 1:
         return base
     elif 0 < exponent < 1:
-        #TO CONTINUE
-        return exponential_function(base, exponent/2)*exponential_function(base, exponent/2)
+        # TO CONTINUE
+        #return exponential_function(base, exponent / 2) * exponential_function(base, exponent / 2)
+        #return base**exponent_a
+        pass
     elif exponent < 0:
         return exponential_function(1 / base, -exponent)
     elif exponent % 2 == 0:
@@ -25,6 +45,14 @@ def exponential_function(base=float, exponent=float):
     else:
         half_exp = exponential_function(base, (exponent - 1) / 2)
         return base * half_exp * half_exp
+
+
+x = exponential_function(2, 1.0002)
+print(x)
+
+
+# val = '%.5f' % (1324343032.324325235)
+# print(val)
 
     # result = 1
     # if base > 0 and power > 0:
@@ -50,5 +78,3 @@ def exponential_function(base=float, exponent=float):
     #     return 1
 
 
-x = exponential_function(2, 0.5)
-print(x)
