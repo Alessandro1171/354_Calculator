9a8be79534b1f4e6130ea8e814d3f2cf29d76425
Li Wah D'Angelo <lwdangelo39@gmail.com>
2023-04-10 16:40:04 -0400

    fixed gamma decimals
    accurate for negative decimals and decimals between 0 and 1. Not as accurate for non-integers above 1

--- GammaFunction.py
+++ GammaFunction.py
@@ -1,13 +1,12 @@
-from HelperFunctions import truncate_value, multi_factorial
+from HelperFunctions import truncate_value, multi_factorial, e_power_x
 from ExponentialFunction import exponential_function
 
 
-# TODO: FIX bug when working with decimals (causes error)
 def gamma(n):
     if n == 0 or n == 1:  # Base case for gamma
         return 1
 
-    if n.is_integer():  # Deal with integers
+    if int(n) == n:  # Deal with integers
         if n > 0:
             return multi_factorial(n - 1, 1)  # Positive integers are just (x-1)!
         else:
@@ -15,28 +14,51 @@ def gamma(n):
 
     # set up constants
     denominator = 1
-    denominator_digits = 1
-    sqrtPI = 1.77245
+    whole_number = int(n)
+    decimal_number = n - whole_number
+
+    # estimated value of gamma for each fraction from 1/2 to 1/8
+    gamma_value = [1.7724538509055160273,
+                   2.6789385347077476337,
+                   3.6256099082219083119,
+                   4.5908437119988030532,
+                   5.5663160017802352043,
+                   6.5480629402478244377,
+                   7.5339415987976119047]
 
     if n > 0:  # Deal with positive decimal numbers
 
-        truncate_value(n)  # Truncate to have 5 decimals
+        # estimate fraction
+        if decimal_number >= 1 / 2:
+            denominator = 2
+        elif decimal_number >= 1 / 3:
+            denominator = 3
+        elif decimal_number >= 1 / 4:
+            denominator = 4
+        elif decimal_number >= 1 / 5:
+            denominator = 5
+        elif decimal_number >= 1 / 6:
+            denominator = 6
+        elif decimal_number >= 1 / 7:
+            denominator = 7
+        elif decimal_number >= 1 / 8:
+            denominator = 8
 
-        if n < 1:
-            return sqrtPI
+        gamma_fraction = gamma_value[denominator - 2]
 
-        for i in range(0, 5, 1):
+        # Calculate the value of gamma using the formula gamma(n+ 1/q) = gamma(1/q) * (qn - n-1)!(q) / q^(n)
+        # Equation found here:
+        # https://en.wikipedia.org/wiki/Particular_values_of_the_gamma_function#General_rational_argument
+        result = gamma_fraction \
+            * multi_factorial(((denominator * whole_number) - denominator - 1), denominator) \
+            / exponential_function(denominator, whole_number)
 
-            if n.is_integer():
-                denominator_digits = i
-                break
-            else:
-                n = 10
-                denominator = 10
+        result = truncate_value(result)
 
-        return sqrtPI * multi_factorial(((denominator * n) - denominator - 1), denominator) / exponential_function(
-            denominator, n)
+        return result
 
     else:  # Deal with negative decimal numbers
-
+        # Uses the property that gamma(n) = 1/n * gamma(n+1), for all non-integers n < 0
+        # Source:
+        # https://math.stackexchange.com/questions/2204020/definition-of-the-gamma-function-for-non-integer-negative-values
         return (1 / n) * gamma(n + 1)
